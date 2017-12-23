05f55834336ad37f1ce9c747d211fc89bcd83abd
Chun Chen <ramichen@tencent.com>
2017-12-23 19:08:34 +0800

    Wait for mapping uid/gid

--- lctn.go
+++ lctn.go
@@ -28,15 +28,21 @@ func Parent() {
 	})
 	args = append(args, flag.Args()...)
 	glog.V(2).Infof("init args %v", args)
+	reader, writer, err := os.Pipe()
+	if err != nil {
+		glog.Fatalf("can't create a pipe: %v", err)
+	}
+	defer writer.Close()
 	cmd := exec.Cmd{
 		Path: os.Args[0],
 		Args: args,
 		SysProcAttr: &syscall.SysProcAttr{
 			Cloneflags: uintptr(syscall.CLONE_NEWNS | syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC | syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWUSER),
 		},
-		Stdin:  os.Stdin,
-		Stdout: os.Stdout,
-		Stderr: os.Stderr,
+		Stdin:      os.Stdin,
+		Stdout:     os.Stdout,
+		Stderr:     os.Stderr,
+		ExtraFiles: []*os.File{reader},
 	}
 	if err := cmd.Start(); err != nil {
 		glog.Fatal(err)
@@ -44,6 +50,10 @@ func Parent() {
 	go func() {
 		if err := mapUidGid(cmd.Process.Pid); err != nil {
 			glog.Fatal(err)
+		}
+		// write 'd' (short for done) message to child process
+		if _, err := writer.Write([]byte{'d'}); err != nil {
+			glog.Fatalf("failed to write into pipe: %v", err)
 		}
 	}()
 	if err := cmd.Wait(); err != nil {
@@ -57,6 +67,16 @@ func Child() {
 	}
 	if err := Mountfs(); err != nil {
 		glog.Fatal(err)
+	}
+	reader := os.NewFile(3, "parent-pipe")
+
+	msg := make([]byte, 1)
+	if _, err := reader.Read(msg); err != nil {
+		glog.Fatalf("failed to read from parent: %v", err)
+	}
+	glog.V(2).Infof("read from parent: %s", string(msg))
+	if err := reader.Close(); err != nil {
+		glog.Warningf("failed to close read pipe: %v", err)
 	}
 	tailArgs := flag.Args()
 	glog.V(1).Infof("container command and args %v", tailArgs)
